7ca85fd965a9d2aa3ff15e6da0dd2f914b1b4460
lusteiger <leonardoxy7@gmail.com>
2020-11-11 00:41:55 +0800

    [xinyu lu] finish the practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,17 @@
-const inject
-export { inject };
+const inject = (source, contents) => {
+          const result = [];
+        
+          for (let i = 0; i < source.length; i++) {
+            const content = contents.find(({ index }) => index === i);
+        
+            if (content) {
+              result.push(content.content);
+            }
+        
+            result.push(source[i]);
+          }
+        
+          return result;
+        };
+        
+        export { inject };
