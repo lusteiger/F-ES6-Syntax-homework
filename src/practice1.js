6638e85138206c6d8aa12eff3e03474203f51a21
lusteiger <leonardoxy7@gmail.com>
2020-11-11 00:40:20 +0800

    [xinyu lu] finish the practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,17 @@
-const parseData
-export { parseData };
+const objects = (keys, values) => {
+          const obj = {};
+        
+          keys.forEach((key, i) => {
+            obj[key] = values[i];
+          });
+        
+          return obj;
+        };
+        
+        const parseData = (source) => {
+          const { data, column } = source;
+          const keys = column.map((c) => c.name);
+          return data.map((values) => objects(keys, values));
+        };
+        
+        export { parseData };
